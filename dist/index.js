f883c572e1ff80e03b1ed176335dff867da49fbe
sakib <mohammadsakib7679@gmail.com>
2024-08-30 01:36:31 +0600

    project setup for blog app

---
+++ dist/index.js
@@ -0,0 +1,52 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const server_1 = require("@apollo/server");
+const standalone_1 = require("@apollo/server/standalone");
+const typeDefs = `
+  type Book {
+    title: String
+    author: String
+  }
+
+  
+  type Query {
+    books: [Book]
+  }
+`;
+const books = [
+    {
+        title: "The Awakening",
+        author: "Kate Chopin",
+    },
+    {
+        title: "City of Glass",
+        author: "Paul Auster",
+    },
+];
+const resolvers = {
+    Query: {
+        books: () => books,
+    },
+};
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new server_1.ApolloServer({
+    typeDefs,
+    resolvers,
+});
+const main = () => __awaiter(void 0, void 0, void 0, function* () {
+    const { url } = yield (0, standalone_1.startStandaloneServer)(server, {
+        listen: { port: 4000 },
+    });
+    console.log(`🚀  Server ready at: ${url}`);
+});
+main();
